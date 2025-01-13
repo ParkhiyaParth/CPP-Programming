fe8140a4a41b3cf31c61fd31f1d78ae326b13ecb
Parth <parkhiyaparth@gmail.com>
2025-01-13 20:06:47 +0530

    Add other codes

---
+++ CodeForces/Problems/Other Contest/C_Find_and_Replace.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if(n==1)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    
+}
+int main()
+{
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
--- CodeForces/Problems/Other Contest/C_Preparing_for_the_Exam.cpp
+++ CodeForces/Problems/Other Contest/C_Preparing_for_the_Exam.cpp
@@ -1,22 +1,43 @@
-#include <iostream>
 #include <bits/stdc++.h>
 #define ll long long
 #define MOD 1000000007
 using namespace std;
 void solve()
 {
-    ll n, m, k;
-    cin >> n >> m >> k;
-    vector<ll> a(n), b(m);
-    for (int i = 0; i < n; i++)
+    int N, M, K;
+    cin >> N >> M >> K;
+    vector<int> A(M), Q(K);
+
+    for (auto &a : A)
+        cin >> a;
+
+    for (auto &q : Q)
+        cin >> q;
+
+    if (K == N)
     {
-        cin >> a[i];
+        cout << string(M, '1') << '\n';
+        return;
     }
-    for (int i = 0; i < m; i++)
+
+    if (K < N - 1)
     {
-        cin >> b[i];
+        cout << string(M, '0') << '\n';
+        return;
     }
-    
+
+    int missing = 0;
+
+    for (int x = 1; x <= N; x++)
+        missing ^= x;
+
+    for (auto &q : Q)
+        missing ^= q;
+
+    for (auto &a : A)
+        cout << (missing == a);
+
+    cout << '\n';
 }
 int main()
 {
